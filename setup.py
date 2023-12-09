9ae37229d7c9b329ca28faf37677b3cdc23c0db9
opsdisk <info@opsdisk.com>
2023-12-09 16:04:07 -0600

    Bumped version to 1.8.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="yagooglesearch",
-    version="1.8.1",
+    version="1.8.2",
     author="Brennon Thomas",
     author_email="info@opsdisk.com",
     description="A Python library for executing intelligent, realistic-looking, and tunable Google searches.",
@@ -13,7 +13,12 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/opsdisk/yagooglesearch",
     packages=setuptools.find_packages(),
-    package_data={"yagooglesearch": ["user_agents.txt", "result_languages.txt"]},
+    package_data={
+        "yagooglesearch": [
+            "user_agents.txt",
+            "result_languages.txt",
+        ],
+    },
     install_requires=[
         "beautifulsoup4>=4.9.3",
         "requests>=2.31.0",
